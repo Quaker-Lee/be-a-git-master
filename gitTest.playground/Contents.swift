4b860dacbef7fc3d14acbbb6dc5ba920a3b7ea3b
Quaker-Lee <tkacl2010@gmail.com>
2024-11-02 12:47:14 +0900

    practice class and function

--- gitTest.playground/Contents.swift
+++ gitTest.playground/Contents.swift
@@ -1,5 +1,24 @@
 import UIKit
 
-var greeting = "Hello, playground"
+let whatIsThis: String = "This project is for git practice freaking git"
 
-var gitTest = "Freaking git"
+class AppleWatch {
+    let series1: String = "Apple Watch Series 1"
+    let series2: String = "Apple Watch Series 2"
+    let series3: String = "Apple Watch Series 3"
+    let series4: String = "Apple Watch Series 4"
+    let series5: String = "Apple Watch Series 5"
+    let series6: String = "Apple Watch Series 6"
+    let series7: String = "Apple Watch Series 7"
+    let series8: String = "Apple Watch Series 8"
+    let series9: String = "Apple Watch Series 9"
+    let series10: String = "Apple Watch Series 10"
+    
+    let ultra1: String = "Apple Watch Ultra 1"
+    let ultra2: String = "Apple Watch Ultra 2"
+}
+
+class swVersion {
+    
+}
+
